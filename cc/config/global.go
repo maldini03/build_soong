c0c807dbcf8fe30c56e8a6eb73ed6c993c8d45c4
Elliott Hughes <enh@google.com>
2022-05-13 17:01:22 -0700

    Try to bump the default to C17
    This has been the default for both gcc and clang for several years.
    
    Bug: http://b/232413369
    Test: treehugger
    Change-Id: I475d1f11049f313cad8cf6c87cfb09e072a12965
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- cc/config/global.go
+++ cc/config/global.go
@@ -298,7 +298,7 @@ var (
 		"-w",
 	}
 
-	CStdVersion               = "gnu11"
+	CStdVersion               = "gnu17"
 	CppStdVersion             = "gnu++17"
 	ExperimentalCStdVersion   = "gnu17"
 	ExperimentalCppStdVersion = "gnu++2a"
